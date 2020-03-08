32fe70ac31e9572385d34c5eb67a416572ab69f9
Ricardo Borba <ricardo_borba06@hotmail.com>
2020-03-07 23:40:18 -0300

    Seeding Service

--- SaleWebMvc/Data/SalesWebMvcContext.cs
+++ SaleWebMvc/Data/SalesWebMvcContext.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-namespace SalesWebMvc.Data
+namespace SalesWebMvc.Models
 {
     public class SalesWebMvcContext : DbContext
     {
-        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
-            : base(options)
+        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options) : base(options)
         {
         }
 
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
